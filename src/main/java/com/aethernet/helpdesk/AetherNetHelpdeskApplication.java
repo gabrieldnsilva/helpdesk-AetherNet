ebc7f13efead75af4e6bab872d33eaa628219e3a
gabrieldnsilva <gabrieldnsilva@gmail.com>
2025-11-11 18:59:15 -0300

    Start Project Setup

---
+++ src/main/java/com/aethernet/helpdesk/AetherNetHelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.aethernet.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetherNetHelpdeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AetherNetHelpdeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aethernet/helpdesk/AetherNetHelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aethernet.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AetherNetHelpdeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
